e404c0834c237f32317d5e9f94e2a1b822201ebc
rotemn2006 <rotemneugeboren@hilma.tech>
2024-11-27 14:27:29 +0200

    cors

--- server/app.js
+++ server/app.js
@@ -9,9 +9,11 @@ const todosRouter = require("./routes/todos");
 const postsRouter = require("./routes/posts");
 const commentsRouter = require("./routes/comments");
 const loginRouter = require("./routes/login.js");
+const cors = require("cors");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
