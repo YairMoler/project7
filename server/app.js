d1da3cc4453887f9dc579665b48ebc162510e0be
YairMoller <yairmoller@hilma.tech>
2024-11-27 11:36:19 +0200

    add routes

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const todosRouter = require("./routes/todos");
+const postsRouter = require("./routes/todos");
+const commentsRouter = require("./routes/comments");
+const loginRouter = require("./routes/login.js");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/todos", todosRouter);
+app.use("/posts", postsRouter);
+app.use("/comments", commentsRouter);
+app, use("/login", loginRouter);
 
 module.exports = app;
